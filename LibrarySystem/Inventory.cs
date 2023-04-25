6d61612497cdefe3485872ee7452c49845a25c59
ty cary <tywincary@gmail.com>
2023-04-24 21:19:26 -0700

    added create book function

--- LibrarySystem/Inventory.cs
+++ LibrarySystem/Inventory.cs
@@ -8,6 +8,16 @@ namespace LibrarySystem
 {
     internal class Inventory
     {
-
+        Dictionary<int, Book> Database = new Dictionary<int, Book>();
+        // Creates a book from input - returns true if successful
+        public bool CreateBook(int isbn, string name, string description, string author,
+            string publisher, string genre, int stock, double price)
+        {
+            if (Database.ContainsKey(isbn))
+                return false;
+            Database.Add(isbn, new Book(
+                isbn, name, description, author, publisher, genre, stock, price));
+            return true;
+        }
     }
 }
