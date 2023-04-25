6eaa4e492d419387de2b123d2eca137c5f9de9d6
ty cary <tywincary@gmail.com>
2023-04-24 20:53:59 -0700

    changed stuff

--- LibrarySystem/Form1.Designer.cs
+++ LibrarySystem/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1896, 1016);
+            Name = "Form1";
+            Text = "Library Management System";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
--- LibrarySystem/Form1.cs
+++ LibrarySystem/Form1.cs
@@ -6,5 +6,10 @@ namespace LibrarySystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
