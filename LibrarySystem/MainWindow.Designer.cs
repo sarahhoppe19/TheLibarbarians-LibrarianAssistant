9821edd6cc0dcb8cbc5fad32d9bade4711054d1c
Tywin Cary <97758444+tycary@users.noreply.github.com>
2023-04-26 10:26:57 -0700

    renamed

--- LibrarySystem/MainWindow.Designer.cs
+++ LibrarySystem/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LibrarySystem
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
--- LibrarySystem/MainWindow.cs
+++ LibrarySystem/MainWindow.cs
@@ -1,10 +1,10 @@
 namespace LibrarySystem
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
         private Inventory LibraryDatabase = new();
         // Basically Main
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
