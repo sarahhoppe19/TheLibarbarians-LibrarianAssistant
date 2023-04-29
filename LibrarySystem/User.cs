af27f41f9576aaad459f69fd022ec6613ab9e62c
ty cary <tywincary@gmail.com>
2023-04-28 17:01:23 -0700

    fixing user

--- LibrarySystem/User.cs
+++ LibrarySystem/User.cs
@@ -10,25 +10,24 @@ namespace LibrarySystem
     {
         //graces user search uses user id as an int
         //also called userID 'uID'
-        public int UID {  get; set; }
+        public int uID {  get; set; }
         public string  Name { get; set; }
         public string Email { get; set; }
         public bool ExistingUser { get; set; }
-        List<Book> CheckedOutBooks { get; set; }
+        List<int> CheckedOutBooks = new();
         
 
         //Default constructor
         public User()
         {
-            UID = 0; //this is not valid
+            uID = 0; //this is not valid
             Name = string.Empty; 
             Email = string.Empty;
-            CheckedOutBooks = new List<Book>();
         }
         //overloaded constructor
-        public User(int uID,  string name, string email, bool existingUser)
+        public User(int id,  string name, string email, bool existingUser)
         {
-            UID = uID;
+            uID = id;
             Name = name;
             Email = email;
             ExistingUser = existingUser;
