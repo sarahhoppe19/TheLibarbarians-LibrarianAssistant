c829f45471251cd8b985d80328988d81dcfb0212
ty cary <tywincary@gmail.com>
2023-04-28 17:30:10 -0700

    added password to user

--- LibrarySystem/User.cs
+++ LibrarySystem/User.cs
@@ -13,24 +13,25 @@ namespace LibrarySystem
         public int ID {  get; set; }
         public string  Name { get; set; }
         public string Email { get; set; }
-        public bool ExistingUser { get; set; }
+        public string Password { get; set; }
         List<int> CheckedOutBooks = new();
         
 
-        //Default constructor
+        // Default constructor
         public User()
         {
-            ID = 0; //this is not valid
+            ID = 0; // this is not valid
             Name = string.Empty; 
             Email = string.Empty;
+            Password = string.Empty;
         }
         //overloaded constructor
-        public User(int id,  string name, string email, bool existingUser)
+        public User(int id,  string name, string email, string password)
         {
             ID = id;
             Name = name;
             Email = email;
-            ExistingUser = existingUser;
+            Password = password;
         }
     }
 }
