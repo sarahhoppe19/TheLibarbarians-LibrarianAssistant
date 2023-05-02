3a7d39b28189a68654ce47eca198b1a79e3a693c
Tywin Cary <97758444+tycary@users.noreply.github.com>
2023-05-01 21:23:45 -0700

    Add files via upload

---
+++ LibrarySystem/TestInventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    internal class TestInventory
+    {
+        Inventory testInventory = new();
+        public void TestCreateBook()
+        {
+            if (!testInventory.CreateBook(123, "ahaha", "advsun\neiaubnvaer", "Maas", "Penguin", "Adventure", 0, 15.00))
+                throw new Exception("Create Book Test Failed");
+        }
+    }
+}
